7b06b70db1cbf1bb2533ca65a2c534860a01f10e
James Lester <resuta566@gmail.com>
2021-06-18 19:45:22 +0800

    models and controllers

---
+++ app/Models/Employees.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Employees extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [ 'firstname', 'lastname', 'company_id', 'email', 'phone'];
+    protected $table = 'employees';
+
+    public function company() 
+    {
+        return $this->belongsTo(Companies::class, 'company_id');
+    }
+}
